b8b967e341431accb5284c0bc9665bd7993772f2
Mash <mashnoon33@gmail.com>
2025-07-14 16:40:10 -0400

    Add the ability to check off ingredients

--- apps/web/src/components/recipie/detail/ingredients/index.tsx
+++ apps/web/src/components/recipie/detail/ingredients/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import type { Ingredient } from '@repo/parser';
-import { RenderIngredientQuantity } from './utils/render-ingedient-quanity';
+"use client";
+import React, { useState } from "react";
+import type { Ingredient } from "@repo/parser";
+import { RenderIngredientQuantity } from "./utils/render-ingedient-quanity";
 
 interface IngredientsProps {
   ingredients: Ingredient[];
@@ -10,23 +11,31 @@ export function Ingredients({ ingredients }: IngredientsProps) {
   return (
     <div className="py-1">
       {ingredients.map((ingredient, index) => (
-        <div
-          className="flex flex-row border-b last:border-none border-gray-300 dark:border-neutral-300/10 justify-between px-4 py-2 min-w-1/2"
-          key={`${ingredient.name}-${index}`}
-        >
-          <div className="font-medium flex">
-            <div className="text-black dark:text-white font-medium">
-              {ingredient.name}
-            </div>
-            {ingredient.description && (
-              <div className="ml-2 text-black/50 dark:text-white/70">
-                {ingredient.description}
-              </div>
-            )}
-          </div>
-          <RenderIngredientQuantity ingredient={ingredient} />
-        </div>
+        <Ingredient key={`${ingredient.name}`} ingredient={ingredient} />
       ))}
+    </div>
+  );
+}
+
+export function Ingredient({ ingredient }: { ingredient: Ingredient }) {
+  const [isChecked, setIsChecked] = useState(false);
+  return (
+    <div
+      className={`min-w-1/2 flex flex-row justify-between border-b hover:cursor-pointer border-gray-300 px-4 py-2 last:border-none dark:border-neutral-300/10 ${isChecked ? 'opacity-20' : ''}`}
+      key={`${ingredient.name}`}
+      onClick={() => setIsChecked(!isChecked)}
+    >
+      <div className="flex font-medium">
+        <div className="font-medium text-black dark:text-white">
+          {ingredient.name}
+        </div>
+        {ingredient.description && (
+          <div className="ml-2 text-black/50 dark:text-white/70">
+            {ingredient.description}
+          </div>
+        )}
+      </div>
+      <RenderIngredientQuantity ingredient={ingredient} />
     </div>
   );
 }
